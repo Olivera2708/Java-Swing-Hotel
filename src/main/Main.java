6aa7f73e134b0a8c68d095023d9046e2aac6a92d
Olivera2708 <olivera.radovanovic2002@gmal.com>
2022-06-02 18:12:25 +0200

    first

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import window.MainFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Podesavanje aplikacije ...");
+		
+		MainFrame main = new MainFrame();
+		
+	}
+}
